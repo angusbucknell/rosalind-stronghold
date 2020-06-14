e9d2f8bb514aad91ebb0cfe754189204a4a79e81
Angus Bucknell <37124633+angusbucknell@users.noreply.github.com>
2020-06-14 22:06:12 +0100

    Included context

--- 01-DNA/01-DNA.py
+++ 01-DNA/01-DNA.py
@@ -1,2 +1,5 @@
+# Given: A DNA string s of length at most 1000 nt.
+# Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in s.
+
 s = input("Input DNA string: ")
 print(s.count("A"), s.count("C"), s.count("G"), s.count("T"))
