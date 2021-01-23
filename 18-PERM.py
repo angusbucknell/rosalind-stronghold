b935017b3254ad7dcdbff4281a027fa04d7fe10a
Angus Bucknell <37124633+angusbucknell@users.noreply.github.com>
2021-01-23 16:47:11 +0000

    Included PERM file

---
+++ 18-PERM.py
@@ -0,0 +1,16 @@
+import math
+from itertools import permutations
+arr = []
+n = int(input("Input integer n<=7: "))
+
+for i in range(0, n):
+    arr.append(str(i+1))
+
+print(int(math.factorial(n)/math.factorial(n-n))) #The nPr formula
+for i in permutations("".join(arr)):
+    print(" ".join(i))
+
+
+
+
+
